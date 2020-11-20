a8449a792a0b8f2a59ac017ee3cd1976986467f7
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-11-20 15:57:06 -0600

    Add files via upload
    Added getter for populating Drop Downs in Feature 2

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ var bodyParser = require('body-parser');
 const CORS=require('cors');
 var port = process.argv[2];
 var path = require('path');
-
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(CORS());
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine);
 app.set('mysql', mysql);
 app.set('view engine', 'handlebars');
 
-app.set('port', 1234);
+
+const filterQuery=require('./sqlF2Filter')
+
 
 app.get('/', (req, res) => {
   res.send('hello world');
@@ -29,6 +30,55 @@ app.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+
+//render activities schedule page, prepopulate dropdowns.
+app.get('/Feature2', function(req, res)
+{
+    console.log('inside get');
+	q=filterQuery.pf
+	var inserts=[req.body.dogID, req.body.overnight, req.body.duration, req.body.stayEnd, req.body.stayStart, req.body.stayCost];
+	mysql.pool.query(q,function (error, results) 
+	{
+		if(error)
+		{
+			console.log(results);
+			res.render('404');
+		}
+		else
+		{	
+			obj={}
+			skills=[]
+			industries=[]
+			courses=[]
+			for (var i=0; i < results.length; i++)
+			{
+				var item=results[i];
+				if (item.course=='course')
+				{
+					courses.push(item.courseName)
+				}
+				else if (item.course =='skill')
+				{
+					skills.push(item.courseName)
+				}
+				else
+				{
+					industries.push(item.courseName)
+				}
+				
+			}
+			
+			obj.skillList=skills
+			obj.courseList=courses
+			obj.indList=industries
+
+			res.render('Feature2',{data:obj});
+		}
+	});
+});
+
+
+
 app.use(function(req,res){
   res.type('text/plain');
   res.status(404);
@@ -41,6 +91,8 @@ app.use(function(err, req, res, next){
   res.status(500);
   res.send('500 - Server Error');
 });
+
+
 
 app.listen(app.get('port'), function(){
   console.log(
