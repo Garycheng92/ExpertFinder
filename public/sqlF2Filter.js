81eede4b5109a2d318fb175edcf1aea9dc8ba453
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-11-20 15:16:19 -0600

    Add files via upload

--- public/sqlF2Filter.js
+++ public/sqlF2Filter.js
@@ -1,55 +1,29 @@
 //Filter queries by filter values -> returns user info
 
-var qbySkillCourseIndustry = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-							  LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-							  INNER JOIN User_Industry ON Users.userID=User_Industry.userID 
-							  INNER JOIN Industry ON User_Industry.industryID=Industry.industryID
-							  INNER JOIN User_Course ON Users.userID=User_Course.userID 
-							  INNER JOIN Course ON User_Course.courseID=Course.courseID
-							  INNER JOIN User_Skill ON Users.userID=User_Skill.userID 
-							  INNER JOIN Skill ON User_Skill.skillID=Skill.skillID
-							  WHERE Course.courseName=? AND Industry.industryName=? AND Skill.skillName=?`
+var qFilterResults= 'SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage '
 
-var qbyCourseIndustry = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-						 LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-						 INNER JOIN User_Industry ON Users.userID=User_Industry.userID 
-						 INNER JOIN Industry ON User_Industry.industryID=Industry.industryID
-						 INNER JOIN User_Course ON Users.userID=User_Course.userID 
-						 INNER JOIN Course ON User_Course.courseID=Course.courseID
-						 WHERE Course.courseName=? AND Industry.industryName=?`
+var qUserProfile= 'FROM Users LEFT JOIN UserProfile ON Users.userID=UserProfile.userID '
 
-var qbySkillIndustry = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-						LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-						INNER JOIN User_Industry ON Users.userID=User_Industry.userID 
-						INNER JOIN Industry ON User_Industry.industryID=Industry.industryID
-						INNER JOIN User_Skill ON Users.userID=User_Skill.userID 
-						INNER JOIN Skill ON User_Skill.skillID=Skill.skillID
-						WHERE Skill.skillName=? AND Industry.industryName=?`
+var qFilterIndustry = 'INNER JOIN User_Industry ON Users.userID=User_Industry.userID INNER JOIN Industry ON User_Industry.industryID=Industry.industryID '
+					   
+var qFilterSkill = 'INNER JOIN User_Skill ON Users.userID=User_Skill.userID INNER JOIN Skill ON User_Skill.skillID=Skill.skillID '
 
-var qbySkillCourse = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-					  LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-					  INNER JOIN User_Course ON Users.userID=User_Course.userID 
-					  INNER JOIN Course ON User_Course.courseID=Course.courseID 
-					  INNER JOIN User_Skill ON Users.userID=User_Skill.userID 
-					  INNER JOIN Skill ON User_Skill.skillID=Skill.skillID
-					  WHERE Course.courseName=? AND Skill.skillName=?`
+var qFilterCourse = 'INNER JOIN User_Course ON Users.userID=User_Course.userID INNER JOIN Course ON User_Course.courseID=Course.courseID '
 
-var qbyCourse = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-				 LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-				 INNER JOIN User_Course ON Users.userID=User_Course.userID 
-				 INNER JOIN Course ON User_Course.courseID=Course.courseID 
-				 WHERE Course.courseName=?`
+var qbySkillCourseIndustry = qFilterResults + qUserProfile + qFilterIndustry + qFilterSkill+ qFilterCourse + 'WHERE Course.courseName=? AND Industry.industryName=? AND Skill.skillName=?'
+	
+var qbyCourseIndustry = qFilterResults + qUserProfile + qFilterIndustry + qFilterCourse+ 'WHERE Course.courseName=? AND Industry.industryName=?'
 
-var qbyIndustry = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users 
-				  LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-				  INNER JOIN User_Industry ON Users.userID=User_Industry.userID 
-				  INNER JOIN Industry ON User_Industry.industryID=Industry.industryID 
-				  WHERE Industry.industryName=?`
+var qbySkillIndustry = qFilterResults + qUserProfile + qFilterIndustry + qFilterSkill + 'WHERE Skill.skillName=? AND Industry.industryName=?'
+
+var qbySkillCourse = qFilterResults + qUserProfile + qFilterCourse + qFilterSkill + 'WHERE Course.courseName=? AND Skill.skillName=?'
+
+var qbyCourse = qFilterResults + qUserProfile + qFilterCourse + 'WHERE Course.courseName=?'
+
+var qbyIndustry = qFilterResults + qUserProfile + qFilterIndustry + 'WHERE Industry.industryName=?'
 				  
-var qbySkill = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users LEFT JOIN UserProfile ON Users.userID=UserProfile.userID 
-			    INNER JOIN User_Skill ON Users.userID=User_Skill.userID 
-			    INNER JOIN Skill ON User_Skill.skillID=Skill.skillID 
-			    WHERE Skill.skillName=?`
+var qbySkill = qFilterResults + qUserProfile + qFilterIndustry + ' WHERE Skill.skillName=?'
 			  
-var qbyNoFilter = `SELECT Users.fName, Users.lName, UserProfile.profileTitle, UserProfile.profileBio, UserProfile.profileImage FROM Users
-				   LEFT JOIN UserProfile ON Users.userID=UserProfile.userID`
+var qbyNoFilter = qFilterResults + qUserProfile
+				   
+var qPopulateFilter=`SELECT courseName,'course' FROM Course UNION SELECT industryName,'industry' FROM Industry UNION SELECT skillName,'skill' From Skill`
