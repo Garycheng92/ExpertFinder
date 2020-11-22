bfaed583995ac200b2230532e5447f8224fe4e26
garycheng92 <garycheng292@gmail.com>
2020-11-21 19:46:40 -0500

    update feature3 route

--- profile_actions.js
+++ profile_actions.js
@@ -1,3 +1,28 @@
+import axios from 'axios';
+
+
+//Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profiles/me');
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    })
+  }
+}
+
+
+
+
+
+
+
 //Get profile by ID
 export const getProfileById = userId => async dispatch => {
 
