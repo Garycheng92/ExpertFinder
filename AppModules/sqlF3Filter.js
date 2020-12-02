e571e847ad98010fa5016357ff1ddb4b86241922
lamcal <lamcal@oregonstate.edu>
2020-12-02 16:53:09 -0600

    Create sqlF3Filter.js
    Query list for F3

---
+++ AppModules/sqlF3Filter.js
@@ -0,0 +1,4 @@
+var qProfileSkill = `(SELECT Users.fName, Skill.skillName, User_Skill.yearsExperience FROM expertfinderdb.Users
+						LEFT JOIN UserProfile ON Users.userID=UserProfile.userID
+						INNER JOIN User_Skill ON Users.UserID=User_Skill.userID
+						INNER JOIN Skill ON User_Skill.skillID=Skill.skillID);`
