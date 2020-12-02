7c05fb4b9de6381d6a0c6f9f161450d1e596f23b
hkm11495 <62815558+hkm11495@users.noreply.github.com>
2020-12-01 19:06:21 -0600

    Add files via upload
    Backend js logic for searching for expert
    Expert.js -> Expert Class

--- AppModules/Expert.js
+++ AppModules/Expert.js
@@ -1,10 +1,17 @@
 class Expert{
-	constructor(first, last, title, bio, img){
+	constructor(id, first, last, title, bio, img, freq){
+		this.user_id=id;
 		this.fName=first;
 		this.lName=last;
 		this.profileTitle=title;
 		this.profileBio=bio;
 		this.profileImage=img;
+		this.page=0
+		this.rank=freq
 	}
+	setPage(pageNum){
+		this.page=pageNum
+	}
+	
 }
 module.exports.Expert=Expert
