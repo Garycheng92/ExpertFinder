357eedf12e5ed6bc0f0fb893dd41686784ff54da
lamcal <lamcal@oregonstate.edu>
2020-12-02 16:56:59 -0600

    Update sqlF3Filter.js

--- AppModules/sqlF3Filter.js
+++ AppModules/sqlF3Filter.js
@@ -1,4 +1,8 @@
-var qProfileSkill = `(SELECT Users.fName, Skill.skillName, User_Skill.yearsExperience FROM expertfinderdb.Users
+var qProfileSkill = `(SELECT Users.userID, Skill.skillName, User_Skill.yearsExperience FROM expertfinderdb.Users
 						LEFT JOIN UserProfile ON Users.userID=UserProfile.userID
 						INNER JOIN User_Skill ON Users.UserID=User_Skill.userID
 						INNER JOIN Skill ON User_Skill.skillID=Skill.skillID);`
+
+var qProfileCourse = ``
+
+var qProfileIndustry = ``
